6a06418cd9e611929320353ab6670cca4329dd74
Marcus Kimpenhaus <kimpenhaus@devil-engineering.de>
2014-06-02 22:19:27 +0200

    added module 1 and module 4 test cases

---
+++ Microsoft.Fakes/Module.1.Configuration/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿namespace Module._1.Configuration
+{
+    using System;
+    using System.Diagnostics;
+    using System.Fakes;
+
+    using Microsoft.QualityTools.Testing.Fakes;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var context = ShimsContext.Create())
+            {
+                ShimDateTime.NowGet = () => new DateTime(1973, 12, 24);
+                Debug.WriteLine("DateTime.Now [shims-context]: {0}", DateTime.Now);
+
+                Debug.WriteLine("DateTime.Now [no shims-context]: {0}", ShimsContext.ExecuteWithoutShims(() => DateTime.Now));
+            }
+        }
+    }
+}
---
+++ Microsoft.Fakes/Module.4.HttpContext/Session123AuthProivder.cs
@@ -0,0 +1,19 @@
+﻿namespace Module._4.HttpContext
+{
+    using HTTPContext = System.Web.HttpContext;
+
+    public class Session123AuthProivder
+    {
+        public static bool IsAuthorized()
+        {
+            bool isAuthorized = false;
+
+            if (null != HTTPContext.Current && null != HTTPContext.Current.Session)
+            {
+                isAuthorized = (string)HTTPContext.Current.Session["auth-token"] == "123";
+            }
+
+            return isAuthorized;
+        }
+    }
+}
